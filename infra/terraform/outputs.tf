f3b09348dede2e3a082661af133fd4a3adfbe5d0
stackgen-automation <automation@stackgen.com>
2024-09-28 09:47:29 +0000

    Automated commit

---
+++ infra/terraform/outputs.tf
@@ -0,0 +1,71 @@
+
+output "aws_s3_shanx_first_serverless_app_bucket_website_endpoint" {
+  value = module.appcd_976676d4-ee68-5dc8-ba29-4a26ee310260.bucket_website_endpoint
+  sensitive = false
+}
+
+
+output "aws_s3_shanx_first_serverless_app_kms_arn" {
+  value = module.appcd_976676d4-ee68-5dc8-ba29-4a26ee310260.kms_arn
+  sensitive = false
+}
+
+
+output "aws_s3_shanx_first_serverless_app_arn" {
+  value = module.appcd_976676d4-ee68-5dc8-ba29-4a26ee310260.arn
+  sensitive = false
+}
+
+
+output "aws_s3_shanx_first_serverless_app_bucket_name" {
+  value = module.appcd_976676d4-ee68-5dc8-ba29-4a26ee310260.bucket_name
+  sensitive = false
+}
+
+
+output "aws_cloudwatch_log_group_aws_lambda_devinfra_serverless_hello_kitty_function_arn" {
+  value = module.appcd_3d6282aa-8e92-5305-b88a-4c023dd2cfbf.arn
+  sensitive = false
+}
+
+
+output "aws_cloudwatch_log_group_aws_lambda_devinfra_serverless_hello_kitty_function_name" {
+  value = module.appcd_3d6282aa-8e92-5305-b88a-4c023dd2cfbf.name
+  sensitive = false
+}
+
+
+output "aws_lambda_shreyansh_serverless_hello_kitty_function_function_arn" {
+  value = module.appcd_d2900d38-6aaf-5f56-b0d8-b909967d568a.function_arn
+  sensitive = false
+}
+
+
+output "aws_lambda_shreyansh_serverless_hello_kitty_function_function_name" {
+  value = module.appcd_d2900d38-6aaf-5f56-b0d8-b909967d568a.function_name
+  sensitive = false
+}
+
+
+output "aws_lambda_shreyansh_serverless_hello_kitty_function_function_url" {
+  value = module.appcd_d2900d38-6aaf-5f56-b0d8-b909967d568a.function_url
+  sensitive = false
+}
+
+
+output "aws_lambda_shreyansh_serverless_hello_kitty_function_invoke_arn" {
+  value = module.appcd_d2900d38-6aaf-5f56-b0d8-b909967d568a.invoke_arn
+  sensitive = false
+}
+
+
+output "aws_iam_role_devinfra_serverless_hello_kitty_role_arn" {
+  value = module.appcd_02abe359-3f4c-560c-be3a-a14d98450ff9.arn
+  sensitive = false
+}
+
+
+output "aws_iam_role_devinfra_serverless_hello_kitty_role_name" {
+  value = module.appcd_02abe359-3f4c-560c-be3a-a14d98450ff9.name
+  sensitive = false
+}
